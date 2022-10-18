36424d7cb59a54a979ff4a0a206888af45522ae4
himanshu01031997 <hs010397@gmail.com>
2022-10-18 08:08:02 +0530

    find maximum no

---
+++ Generic/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericFindMax
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FindMaxNo findMaxNo = new FindMaxNo();
+            findMaxNo.MaximumNum(10,20,30);
+        }
+    }
+}
