e5fbb1f93b50bd18fec8438f2754c7440a79dc73
himanshu01031997 <hs010397@gmail.com>
2022-10-18 10:40:59 +0530

    maximum float value

--- Generic/Program.cs
+++ Generic/Program.cs
@@ -1,11 +1,12 @@
-﻿namespace GenericFindMax
+﻿using System.Collections;
+namespace GenericFindMax
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            FindMaxNo findMaxNo = new FindMaxNo();
-            findMaxNo.MaximumNum(10,20,30);
+            FindMaxFloat findMaxfloat = new FindMaxFloat();
+            findMaxfloat.MaximumFloat(21.3, 4.5, 6.7);
         }
     }
 }
